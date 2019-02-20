f43368cc205f5cab343b8f47cf00b41ee58c0d93
Arman Babakhani <armanxbabakhani@berkeley.edu>
2019-02-19 16:39:14 -0800

    The Run script is completed
    Note: For the data to be automatically sorted after running the bash script, it must be made sure that the input file is the only text file in the input folder.

--- Run.sh
+++ Run.sh
@@ -1,2 +1,12 @@
-cd input/
-python 
+MAIN_DIR=`pwd`
+cd $MAIN_DIR/input
+
+find . -name "*.txt" -exec python3 $MAIN_DIR/src/Arman_pharmacy.py {} \;
+
+if [ -e top_cost_drug.txt ];
+then
+        mv top_cost_drug.txt $MAIN_DIR/output/
+        echo "The output file is successfully generated. Your data is sorted!"
+else
+        echo "The output file is not found. Something might be wrong in the code!"
+fi
